5217d3669f93fceeb76036eb1a85656d0753c33e
Miqueias <m.meneses@uni9.edu.br>
2021-04-29 00:57:35 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,170 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "myTFSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "myTFPublicIP"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myTFNSG"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+ 
+}
+
+# Create network interface
+resource "azurerm_network_interface" "nic" {
+  name                      = "myNIC"
+  location                  = "westus2"
+  resource_group_name       = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myNICConfg"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
+
+# Create a Linux virtual machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "myTFVM"
+  location              = "westus2"
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "myOsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+  }
+ 
+  os_profile {
+    computer_name  = "impacta"
+    admin_username = "miqueias"
+    admin_password = "(Impacta4r"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+data "azurerm_public_ip" "ip" {
+  name                = azurerm_public_ip.publicip.name
+  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
+  depends_on          = [azurerm_virtual_machine.vm]
+}
+
+
+
+
+resource "azurerm_mysql_server" "example" {
+  name                = "mysqlse"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  administrator_login          = "mysqladminun"
+  administrator_login_password = "H@Sh1CoR3!"
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
+# resource "null_resource" "apt" {
+#   # Changes to any instance of the cluster requires re-provisioning
+#   triggers = {
+#     order = azurerm_virtual_machine.vm.id
+#   }
+
+# #   # Bootstrap script can run on any instance of the cluster
+# #   # So we just choose the first in this case
+
+
+#   provisioner "remote-exec" {
+#       connection {
+#          type = "ssh"
+#          user = "miqueias"
+#          password = "(Impacta4r"
+#          host = azurerm_public_ip.publicip.ip_address
+#       }
+#     # Bootstrap script called with private_ip of each node in the cluster
+#     inline = [
+#      "sudo apt-get update", "sudo apt-get install -y software-properties-common","sudo apt-add-repository --yes --update ppa:ansible/ansible","sudo apt-get -y install python3 ansible"
+#     ]
+#   }
+
